95750690d6bdf0ca86942275eda42262f5de350b
Tarmstrong95 <triston95strong@gmail.com>
2019-10-06 14:24:14 +0700

    Move navigation

--- better-friends/src/App.js
+++ better-friends/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Container from './Components/login/container'
-import Main from './Components/main/index'// eslint-disable-next-line
+import Main from './Components/main/index'
 import PrivateRoute from './Components/PrivateRoute'
 import './App.css'
 import {connect} from 'react-redux'
-import {login, logout} from './Actions/'
 import { Route, Link } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css'
+import Navig from './Components/nav/nav';
+
 
 
 
@@ -13,18 +15,9 @@ import { Route, Link } from 'react-router-dom';
 const App = props => {
 
 
-if(localStorage.getItem("token")){
-  props.login();
-}
-
   return (
-    <div className='display'>
-      <nav className="main-nav">
-      {props.isLoggedIn && <Link onClick={() => props.logout()} className="main-nav-link" to='/cred/login'> Logout</Link>}
-        {!props.isLoggedIn && <Link className="main-nav-link" to='/cred/login'> Login</Link>}
-        <Link className="main-nav-link" to='/protected'> Events</Link>
-        {props.isLoggedIn && <Link className="main-nav-link" >{localStorage.getItem('firstname')}</Link>}
-      </nav>
+    <div >
+      <Navig/>
       <Route path='/cred' render={props => <Container {...props} loggedin={props.isLoggedIn} /> }/>
       <PrivateRoute path='/protected' component={Main} />
       {/* switch to Route to view page*/}
@@ -38,4 +31,4 @@ const mapStateToProps = state => {
     isLoggedIn: state.isLoggedIn
   }
 }
-export default connect(mapStateToProps, {login, logout})(App);
+export default connect(mapStateToProps, {})(App);
